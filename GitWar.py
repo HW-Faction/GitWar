9fe25f7a19dd5a84478d7dd2c46cb061ccfe6e2e
HW-Faction <hankwilliamson49@gmail.com>
2019-09-26 15:05:59 +0530

    v4.0

--- GitWar.py
+++ GitWar.py
@@ -25,7 +25,32 @@ source1 = open('./Source1.py', 'r')
 source2 = open('Source2.py', 'a')
 linelist = source1.readlines()
 countOfLines = len(linelist)
-for i in range(countOfLines):
+for i in range(1):
+    print('saving line', i)
+    source2.write('' + linelist[i])
+    print('committing to github...')
+    os.system("git commit -m 'v4.0' .")
+    for i in progressbar(range(15), "Computing: ", 40):
+        time.sleep(0.1)
+    print('pushing to master...')
+
+    for i in tqdm(range(int(9e6))):
+        pass
+    os.system('git push origin master')
+    for i in progressbar(range(15), "Computing: ", 40):
+        time.sleep(0.1)
+    for i in tqdm(range(int(9e6))):
+        pass
+    print('\n' + '#'*20 + ' ---DONE--- ' + '#'*20 + '\n')
+
+source1.close()
+source2.close()
+
+source1 = open('./Source1.py', 'r')
+source2 = open('Source2.py', 'a')
+linelist = source1.readlines()
+countOfLines = len(linelist)
+for i in range(1):
     print('saving line', i)
     source2.write('' + linelist[i])
     print('committing to github...')
--- Source2.py
+++
@@ -1,2 +0,0 @@
-with open('./Source1.py', 'r') as source1:
-    with open('Source2.py', 'a') as source2:
