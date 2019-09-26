0ba00c9875da8aa07ab4ac64a8cfc022259b010e
HW-Faction <hankwilliamson49@gmail.com>
2019-09-26 14:09:49 +0530

    v1.0

---
+++ GitWar.py
@@ -0,0 +1,34 @@
+import time
+import os
+from tqdm import tqdm
+import sys
+import subprocess
+
+
+""" def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" %
+                   (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
+
+for i in progressbar(range(15), "Computing: ", 40):
+    time.sleep(0.1) """
+
+with open('./Source1.py', 'r') as source1:
+    with open('Source2.py', 'a') as source2:
+        linelist = source1.readlines()
+        countOfLines = len(linelist)
+        for i in range(countOfLines):
+            source2.write('' + linelist[i])
+            print('committing to github...')
+            print('pushing to master...')
+            for i in tqdm(range(int(9e6))):
