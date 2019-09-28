fa2104178167cda8d5a2a27a0e2eac443d950d7d
HW-Faction <hankwilliamson49@gmail.com>
2019-09-28 21:14:31 +0530

    v4.0

--- Source2.py
+++ Source2.py
@@ -6,3 +6,4 @@ with open('./Source1.py', 'r') as source1:
 with open('./Source1.py', 'r') as source1:
 with open('./Source1.py', 'r') as source1:
 with open('./Source1.py', 'r') as source1:
+with open('./Source1.py', 'r') as source1:
