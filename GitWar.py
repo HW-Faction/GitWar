4f03c851786204a37babb8d9be427cab1162a1a5
HW-Faction <hankwilliamson49@gmail.com>
2019-10-02 20:17:41 +0530

    v4.0

--- GitWar.py
+++ GitWar.py
@@ -21,7 +21,7 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
     file.flush()
 
 
-for i in range(46):
+for i in range(31):
     source1 = open('./Source1.py', 'r')
     source2 = open('Source2.py', 'a')
     linelist = source1.readlines()
