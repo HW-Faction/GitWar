c84e0987c0c20e910dec6268b2b8f79595c1f992
HW-Faction <hankwilliamson49@gmail.com>
2019-10-12 08:28:29 +0530

    v4.0

--- Source2.py
+++ Source2.py
@@ -220,3 +220,4 @@ with open('./Source1.py', 'r') as source1:
         for i in range(countOfLines):
             source2.write('' + linelist[i])
             source2.write('#'*25 + '\n')
+with open('./Source1.py', 'r') as source1:
