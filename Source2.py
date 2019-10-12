c2501f8923dd849dba53ae4b472813165570f7e0
HW-Faction <hankwilliamson49@gmail.com>
2019-10-12 08:21:30 +0530

    v4.0

--- Source2.py
+++ Source2.py
@@ -179,3 +179,4 @@ with open('./Source1.py', 'r') as source1:
         linelist = source1.readlines()
         countOfLines = len(linelist)
 with open('./Source1.py', 'r') as source1:
+    with open('Source2.py', 'a') as source2:
