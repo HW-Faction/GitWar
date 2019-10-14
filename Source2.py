87a9fce837efb9e5aadbc872bc4800081cb32223
HW-Faction <hankwilliamson49@gmail.com>
2019-10-14 18:59:50 +0530

    v4.0

--- Source2.py
+++ Source2.py
@@ -253,3 +253,4 @@ with open('./Source1.py', 'r') as source1:
         for i in range(countOfLines):
             source2.write('' + linelist[i])
 with open('./Source1.py', 'r') as source1:
+    with open('Source2.py', 'a') as source2:
