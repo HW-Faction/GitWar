9ea81378c9f5d50041cd68d5b6f2a48115e0347b
HW-Faction <hankwilliamson49@gmail.com>
2019-10-15 23:25:30 +0530

    v4.0

--- Source2.py
+++ Source2.py
@@ -284,3 +284,4 @@ with open('./Source1.py', 'r') as source1:
     with open('Source2.py', 'a') as source2:
 with open('./Source1.py', 'r') as source1:
     with open('Source2.py', 'a') as source2:
+        linelist = source1.readlines()
